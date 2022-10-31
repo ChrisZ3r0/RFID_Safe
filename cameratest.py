ddeb0ead10816e512bb6a8ab22279cee08d80b09
ChrisZ3r0 <kollar001031@gmail.com>
2022-10-31 14:34:14 +0100

    tested macro pictures

--- cameratest.py
+++ cameratest.py
@@ -1,4 +1,4 @@
 import os
 from time import sleep
 
-out = os.system("/usr/bin/libcamera-still --vf -t 10000 -o cica.jpg")
+out = os.system("/usr/bin/libcamera-still  --hf --vf -t 10000 -o buttontest.jpg")
