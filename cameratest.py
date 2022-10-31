39f5e7447fad54936734b4195a86f2e191609030
ChrisZ3r0 <kollar001031@gmail.com>
2022-10-31 15:35:31 +0100

    testing out libcamera python script instead of using system wide terminal commands

--- cameratest.py
+++ cameratest.py
@@ -1,4 +1,8 @@
-import os
-from time import sleep
+from picamera2 import Picamera2, Preview
 
-out = os.system("/usr/bin/libcamera-still  --hf --vf -t 10000 -o buttontest.jpg")
+picam2 = Picamera2()
+i=2
+picam2.start_preview(Preview.QTGL, x=100, y=200, width=800, height=600)
+picam2.start()
+while True:
+    i
