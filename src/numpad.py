c8571eb56a98a58c8ab942338c2b93ad77cc1e46
ChrisZ3r0 <kollar001031@gmail.com>
2022-11-07 22:08:17 +0100

    remaked the numpad.py code asking system, made a password and the txt file that contains it

--- src/numpad.py
+++ src/numpad.py
@@ -13,7 +13,6 @@ C1 = 12
 C2 = 16
 C3 = 20
 C4 = 21
-
 # Initialize the GPIO pins
 
 GPIO.setwarnings(False)
@@ -40,18 +39,27 @@ def main():
             numOrCharacterGot = ""
 
             numOrCharacterGot = readLine(L1, ["1", "2", "3", "A"])
-            numOrCharacterGot = readLine(L2, ["4", "5", "6", "B"])
-            numOrCharacterGot = readLine(L3, ["7", "8", "9", "C"])
-            numOrCharacterGot = readLine(L4, ["*", "0", "#", "D"])
-
+            if not numOrCharacterGot: numOrCharacterGot = readLine(L2, ["4", "5", "6", "B"])
+            if not numOrCharacterGot: numOrCharacterGot = readLine(L3, ["7", "8", "9", "C"])
+            if not numOrCharacterGot: numOrCharacterGot = readLine(L4, ["*", "0", "#", "D"])
+            
+            # numOrCharacterGot = readLine(L1, ["1", "2", "3", "A"])
+            # numOrCharacterGot = readLine(L2, ["4", "5", "6", "B"])
+            # numOrCharacterGot = readLine(L3, ["7", "8", "9", "C"])
+            # numOrCharacterGot = readLine(L4, ["*", "0", "#", "D"])
+            
             pinBefore += numOrCharacterGot
-            if numOrCharacterGot and pinBefore == 4:
-                if pinIsValid(int(pinBefore)):
-                    pass
+            print(pinBefore)
+            if numOrCharacterGot and len(pinBefore) == 4:
+                if pinIsValid(pinBefore):
+                    print("it is valid")
+                    pinBefore = ""
                 else:
-                    pass
+                    print("it is not valid")
+                    pinBefore = ""
             else:
-                time.sleep(1)
+                time.sleep(0.1)
+            if numOrCharacterGot: time.sleep(0.5)
     except KeyboardInterrupt:
         print("\nApplication stopped!")
 
@@ -63,21 +71,30 @@ def main():
 # to the detected column
 def readLine(line, characters):
     GPIO.output(line, GPIO.HIGH)
-    if GPIO.input(C1) == 1:
-        print(characters[0])
-    if GPIO.input(C2) == 1:
-        print(characters[1])
-    if GPIO.input(C3) == 1:
-        print(characters[2])
-    if GPIO.input(C4) == 1:
-        print(characters[3])
+    if GPIO.input(C1):
+        # print(characters[0])
+        return characters[0]
+    if GPIO.input(C2):
+        # print(characters[1])
+        return characters[1]
+    if GPIO.input(C3):
+        # print(characters[2])
+        return characters[2]
+    if GPIO.input(C4):
+        # print(characters[3])
+        return characters[3]
     GPIO.output(line, GPIO.LOW)
 
     return ""
 
 
 def pinIsValid(pin):
-    return True
+    with open("/home/chris/RFID_Safe/src/password/pwd.txt", "r") as f:
+        password = f.readline()
+    
+    if password == pin:
+        return True
+    return False
 
 
 if __name__ == "__main__":
