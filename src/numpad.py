5a2ed9d0497731a913533ec500f97dcda126d0e0
tzsombi01 <toreky.zsombor@gmail.com>
2022-11-07 19:41:48 +0100

    Formatted numpad.py

--- src/numpad.py
+++ src/numpad.py
@@ -37,25 +37,32 @@ GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # If it detects a change, the user pressed the button that connects the given line
 # to the detected column
 
+
 def readLine(line, characters):
     GPIO.output(line, GPIO.HIGH)
-    if(GPIO.input(C1) == 1):
+    if GPIO.input(C1) == 1:
         print(characters[0])
-    if(GPIO.input(C2) == 1):
+    if GPIO.input(C2) == 1:
         print(characters[1])
-    if(GPIO.input(C3) == 1):
+    if GPIO.input(C3) == 1:
         print(characters[2])
-    if(GPIO.input(C4) == 1):
+    if GPIO.input(C4) == 1:
         print(characters[3])
     GPIO.output(line, GPIO.LOW)
 
-try:
-    while True:
-        # call the readLine function for each row of the keypad
-        readLine(L1, ["1","2","3","A"])
-        readLine(L2, ["4","5","6","B"])
-        readLine(L3, ["7","8","9","C"])
-        readLine(L4, ["*","0","#","D"])
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    print("\nApplication stopped!")
+
+def main():
+    try:
+        while True:
+            # call the readLine function for each row of the keypad
+            readLine(L1, ["1", "2", "3", "A"])
+            readLine(L2, ["4", "5", "6", "B"])
+            readLine(L3, ["7", "8", "9", "C"])
+            readLine(L4, ["*", "0", "#", "D"])
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\nApplication stopped!")
+
+
+if __name__ == "__main__":
+    main()
